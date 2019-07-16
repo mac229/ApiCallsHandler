2f9d4c8e1d30aa100b6da8c8369fa39badfbdf57
Maciej Kozłowski <maciej.kozlowski@espeo.eu>
2019-07-16 14:29:33 +0200

    ApiResponse as data classes

--- apihandler/src/main/java/com/maciejkozlowski/apihandler/response/ApiResponse.kt
+++ apihandler/src/main/java/com/maciejkozlowski/apihandler/response/ApiResponse.kt
@@ -4,6 +4,6 @@ import com.maciejkozlowski.apihandler.errors.ApiError
 
 sealed class ApiResponse<T> {
 
-    class Success<T>(val data: T) : ApiResponse<T>()
-    class Error<T>(val error: ApiError) : ApiResponse<T>()
+    data class Success<T>(val data: T) : ApiResponse<T>()
+    data class Error<T>(val error: ApiError) : ApiResponse<T>()
 }
--- apihandler/src/main/java/com/maciejkozlowski/apihandler/response/CompletableApiResponse.kt
+++ apihandler/src/main/java/com/maciejkozlowski/apihandler/response/CompletableApiResponse.kt
@@ -5,5 +5,5 @@ import com.maciejkozlowski.apihandler.errors.ApiError
 sealed class CompletableApiResponse {
 
     object Complete : CompletableApiResponse()
-    class Error(val error: ApiError) : CompletableApiResponse()
+    data class Error(val error: ApiError) : CompletableApiResponse()
 }
